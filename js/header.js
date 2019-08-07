ae6f2c18d7ec2fdaf5aeca65efaae96f563d8316
Prasham Jain <prshmjain1@gmail.com>
2019-08-07 20:28:20 +0530

    Added buttons and comment box in Post.html

---
+++ js/header.js
@@ -0,0 +1,13 @@
+function clear_signInModal()
+{
+    document.getElementById("username_signIn").value = "";
+    document.getElementById("pass_signIn").value = "";
+    
+}
+function clear_signUpModal()
+{
+    document.getElementById("fullname").value = "";
+    document.getElementById("username").valuw = "";
+    document.getElementById("pass").value = "";
+    document.getElementById("confirm_pass").value = "";
+}
---
+++ js/post.js
@@ -0,0 +1,10 @@
+function edit()
+{
+    document.getElementById("edit_button").style.display = "none";
+    document.getElementById("save_button").style.display = "block";
+}
+function save()
+{
+    document.getElementById("save_button").style.display = "none";
+    document.getElementById("edit_button").style.display = "block";
+}
